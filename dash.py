1450f2983040de893b267678f1e3c633a73533e6
Charlesdev <chinaskicharles0@gmail.com>
2023-11-17 17:57:47 -0300

    Streamlit App

--- dash.py
+++ dash.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(page_title='VecinoNet(Redes para Todos)', page_icon = ':bar_chart:', layout = 'centered')
 
-# Título centradonusando HTML
+# Título centradon usando 
 st.markdown("<h1 style='text-align: center'>VecinoNet(Redes para Todos)</h1><hr style='height:2px;border-width:0;color:gray;background-color:gray'>", unsafe_allow_html=True)
 
 # Contenido centrado
